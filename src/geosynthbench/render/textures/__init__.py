d9169ef22289411597eb4c1aafe83a5404dc8fd3
Antoine LI <91544027+antoinelii@users.noreply.github.com>
2026-02-24 01:11:31 +0100

    refactor(textures): reorganize imports and define __all__ for better textures module structure

--- src/geosynthbench/render/textures/__init__.py
+++ src/geosynthbench/render/textures/__init__.py
@@ -1,1 +1,44 @@
 # src/geosynthbench/render/textures/__init__.py
+
+from .compose import alpha_over, render_full_rgb
+from .noise import fbm_2d, value_noise_2d
+from .palettes import (
+    building_palette,
+    roads_palette,
+    settlement_palette,
+    vegetation_palette,
+    water_palette,
+)
+from .params import (
+    BuildingParams,
+    RoadParams,
+    SceneParams,
+    SettlementParams,
+    VegetationParams,
+    WaterParams,
+)
+from .post import postprocess
+from .shadows import add_building_shadows
+from .terrain import background_palette_from_elevation, hillshade
+
+__all__ = [
+    "alpha_over",
+    "background_palette_from_elevation",
+    "fbm_2d",
+    "hillshade",
+    "render_full_rgb",
+    "value_noise_2d",
+    "water_palette",
+    "roads_palette",
+    "settlement_palette",
+    "vegetation_palette",
+    "building_palette",
+    "add_building_shadows",
+    "postprocess",
+    "SceneParams",
+    "WaterParams",
+    "VegetationParams",
+    "RoadParams",
+    "SettlementParams",
+    "BuildingParams",
+]
